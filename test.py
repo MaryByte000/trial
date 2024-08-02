6e108f8360ee5c6ab0bdb4270052af53258ea62c
MaryByte000 <marykristinecaneda@gmail.com>
2024-08-02 17:35:07 +0800

    halo

---
+++ test.py
@@ -0,0 +1,17 @@
+print("gwapa ko")
+
+number_one = 10 
+number_two = 13
+
+first_name = 'Mary'
+
+char_sample = 'c'
+
+people_age = {
+    'Mary': 21,
+    'Mel': 20,
+    'Nat': 20
+}
+
+total = number_one + number_two
+print('the resut is', total)
